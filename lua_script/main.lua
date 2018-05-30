1f38e2ab32b6435b20a051e157b94f5894c9829d
xiaol-luo <xiaol.luo@163.com>
2018-05-30 10:58:25 +0800

    add lua lib lpeg

---
+++ lua_script/main.lua
@@ -0,0 +1,18 @@
+
+local function parse_cmd_arg(out_ret)
+    out_ret = out_ret or {}
+
+    parse_fns = {}
+    parse_fns["-package_path"] = function(args, arg_idx, ret)
+
+    end
+    parse_fns["-package_cpath"] = function(args, arg_idx, ret)
+
+    end
+
+    for arg_idx=1, #arg do
+        curr_arg = arg[arg_idx]
+        
+    end
+end
+
