641081632dd5b91d87087a3994d601501f4c387b
xiaol-luo <xiaol.luo@163.com>
2018-12-19 22:40:27 +0800

    use macro to make lib

--- cmake/AllInclude.cmake
+++
@@ -1,3 +0,0 @@
-include (${CMAKE_SOURCE_DIR}/cmake/Define.cmake)
-include (${CMAKE_SOURCE_DIR}/cmake/Utils.cmake)
-# include (${CMAKE_SOURCE_DIR}/cmake/Libs.cmake)
---
+++ cmake/test/test_macro.cmake
@@ -0,0 +1,12 @@
+MACRO(test_macro p1 p2 p3)
+	MESSAGE(STATUS "NOT WRAP " p1 " " p2 " " p3)
+	MESSAGE(STATUS "WRAP " "${p1} ${p2} ${p3}")
+ENDMACRO(test_macro)
+
+SET(a "hello")
+SET(b "world")
+SET(c "d")
+SET(d "this is d")
+test_macro(a b c)
+MESSAGE(STATUS "-------")
+test_macro(${a} ${b} ${c})
