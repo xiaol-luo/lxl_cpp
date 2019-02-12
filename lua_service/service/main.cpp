8332552abb3d2a914e8528aab3cef4ce64a108e8
xiaol-luo <xiaol.luo@163.com>
2019-02-12 19:50:41 +0800

    add windows mongo cxx drive

--- lua_service/service/main.cpp
+++ lua_service/service/main.cpp
@@ -144,6 +144,41 @@ void StartLuaScript(lua_State *L, int argc, char **argv)
 	}
 }
 
+#include <mongocxx/client.hpp>
+#include <mongocxx/database.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+
+void try_mongodb_api()
+{
+	mongocxx::instance ins{};
+	{
+		mongocxx::uri mg_uri("mongodb://192.168.56.101:27017");
+		mongocxx::client mg_client(mg_uri);
+		mongocxx::database test_db = mg_client["test"];
+		auto builder = bsoncxx::builder::stream::document();
+		bsoncxx::document::value doc_value = builder
+			<< "name" << "MongoDB"
+			<< "type" << "database"
+			<< "count" << 1
+			<< "versions" << bsoncxx::builder::stream::open_array
+			<< "v3.2" << "v3.0" << "v2.6"
+			<< bsoncxx::builder::stream::close_array
+			<< "info" << bsoncxx::builder::stream::open_document
+			<< "x" << 203
+			<< "y" << 102
+			<< bsoncxx::builder::stream::close_document
+			<< bsoncxx::builder::stream::finalize;
+
+
+		mongocxx::collection test_coll = test_db["test_coll"];
+		bsoncxx::stdx::optional<mongocxx::result::insert_one> result = test_coll.insert_one(doc_value.view());
+
+	}
+}
+
+
 int main (int argc, char **argv) 
 {
 #ifdef WIN32
@@ -155,6 +190,8 @@ int main (int argc, char **argv)
 	signal(SIGINT, QuitGame);
 	signal(SIGPIPE, SIG_IGN);
 #endif
+
+	// try_mongodb_api();
 
 	// argv: exe_name work_dir lua_file lua_file_params...
 	if (argc < 3)
