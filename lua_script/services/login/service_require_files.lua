ac41da8f87834657eb7d42a1484c153261444249
luoxiaolong <luoxiaolong@corp.netease.com>
2019-05-22 20:58:11 +0800

    rename some files

--- lua_script/services/login/service_require_files.lua
+++ lua_script/services/login/service_require_files.lua
@@ -1,8 +1,8 @@
 local files = {
     "services.login.login_zone_service_msg_handler",
-    "services.login.service_main_mongo",
-    "services.login.service_main_msg_handler",
-    "services.login.service_main_rpc_mgr",
+    "services.login.service_main__mongo",
+    "services.login.service_main__msg_handler",
+    "services.login.service_main__rpc_mgr",
     "services.login.service_main__setup_logics",
 }
 
