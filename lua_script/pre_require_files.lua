56ad73ef42f16f564838816d8b460eac49851d2d
luoxiaolong <luoxiaolong@corp.netease.com>
2019-06-14 16:22:36 +0800

    add reload module

--- lua_script/pre_require_files.lua
+++ lua_script/pre_require_files.lua
@@ -43,6 +43,7 @@ local files = {
     "libs.etcd.etcd_client_op_set",
     "libs.etcd.etcd_client_result",
     "libs.etcd.etcd_client_cxx",
+    "libs.reload",
     "common.date_time",
     "common.zone_service_mgr.zone_service_mgr_def",
     "common.zone_service_mgr.zone_service_mgr",
