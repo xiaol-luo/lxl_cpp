21aee07fab2983184f4593c09f62c7f35a9ec1eb
xiaol-luo <xiaol.luo@163.com>
2019-08-30 00:26:46 +0800

    improve mongo cluster demo

--- tools/manage_mongo_cluster/demo/mongo_cluster/start_all.sh
+++ tools/manage_mongo_cluster/demo/mongo_cluster/start_all.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 source /shared/mongo_cluster/config.sh
+
+typeset -l low_case_str
+is_init=false
+if [ $# > 1 ];then
+	low_case_str=$1
+	if [ ${low_case_str} = "init" ];then
+		is_init=true
+	fi
+fi
+
 pre_dir=`pwd`
 cd ${root_dir}
 
@@ -19,7 +29,22 @@ do
     done
 done
 
+if [ ${is_init} = true ];then
+	mongo -port 9000 -eval 'rs.initiate({ _id:"rs_cfg", members:[ {_id:0, host:"127.0.0.1:9000"}, {_id:1, host:"127.0.0.1:9001"}, {_id:2, host:"127.0.0.1:9002"} ] }); rs.slaveOk()' 
+	mongo -port 9100 -eval 'rs.initiate({ _id:"rs_1", members:[ {_id:0, host:"127.0.0.1:9100"}, {_id:1, host:"127.0.0.1:9101"}, {_id:2, host:"127.0.0.1:9102"} ] }); rs.slaveOk()'
+	mongo -port 9200 -eval 'rs.initiate({ _id:"rs_2", members:[ {_id:0, host:"127.0.0.1:9200"}, {_id:1, host:"127.0.0.1:9201"}, {_id:2, host:"127.0.0.1:9202"} ] }); rs.slaveOk()'
+	mongo -port 9300 -eval 'rs.initiate({ _id:"rs_3", members:[ {_id:0, host:"127.0.0.1:9300"}, {_id:1, host:"127.0.0.1:9301"}, {_id:2, host:"127.0.0.1:9302"} ] }); rs.slaveOk()'
+fi
+
 mongos -f "mongos_${mongos_from}.conf"
+
+if [ ${is_init} = true ];then
+	mongo -port 9400 -eval 'sh.addShard("rs_1/127.0.0.1:9100,127.0.0.1:9101,127.0.0.1:9102")'
+	mongo -port 9400 -eval 'sh.addShard("rs_2/127.0.0.1:9200,127.0.0.1:9201,127.0.0.1:9202")'
+	mongo -port 9400 -eval 'sh.addShard("rs_3/127.0.0.1:9300,127.0.0.1:9301,127.0.0.1:9302")'
+	#make collection shard
+	mongo -port 9400 admin -eval 'db.runCommand({"enablesharding":"testsh"}); db.runCommand({"shardcollection":"testsh.role","key":{_id:"hashed"}})'
+fi
 
 sh ps.sh
 
