548f98e19274702542c796c341c02368e1a580da
xiaol-luo <root@localhost.localdomain>
2019-09-04 10:43:36 +0800

    modify auto gen service tool

--- tools/manage_service/opera/create.py
+++ tools/manage_service/opera/create.py
@@ -180,7 +180,7 @@ def create_zone(parse_ret):
         config.Service_Type.robot: ServiceHelper("robot", parse_ret, setting),
         config.Service_Type.match: ServiceHelper("match", parse_ret, setting),
         config.Service_Type.fight: ServiceHelper("fight", parse_ret, setting),
-        config.Service_Type.fight: ServiceHelper("room", parse_ret, setting),
+        config.Service_Type.room: ServiceHelper("room", parse_ret, setting),
     }
     service_Infos = []
     for service_type, service_data in service_helps.items():
