9199895feb6e9658279b8e414197e9531ef2b108
xiaol-luo <xiaol.luo@163.com>
2020-07-01 01:27:05 +0800

    skip list

--- server/third_party/data_struct/skip_list/skip_list.cpp
+++ server/third_party/data_struct/skip_list/skip_list.cpp
@@ -1,17 +1,52 @@
 #include "skip_list.h"
 #include <stdlib.h>
+#include <string.h>
 
-skip_list_node_t * skip_list_node_alloc(void * key, void * data, uint32_t  lvl_node_size, skip_list_free_node_key_pt free_key)
+static bool skip_list_node_check_expand_forward_nodes(skip_list_node_t *node, uint16_t new_size)
+{
+	if (new_size <= node->forward_node_size)
+		return false;
+
+	uint16_t old_node_size = node->forward_node_size;
+	skip_list_node_t **old_nodes = node->forward_nodes;
+	node->forward_node_size = new_size;
+	node->forward_nodes = (skip_list_node_t **)calloc(new_size, sizeof(skip_list_node_t *));
+	if (old_nodes)
+	{
+		memcpy(node->forward_nodes, old_nodes, sizeof(skip_list_node_t *) * old_node_size);
+		free(old_nodes); old_nodes = NULL;
+	}
+	return true;
+}
+
+static void skip_list_set_using_max_lvl(skip_list_t * list, uint16_t lvl)
+{
+	if (NULL == list)
+		return;
+
+	if (list->using_max_lvl < lvl)
+	{
+		if (skip_list_node_check_expand_forward_nodes(list->head, lvl + 1) || skip_list_node_check_expand_forward_nodes(list->tail, lvl + 1))
+		{
+			for (uint16_t i = list->using_max_lvl; i <= lvl; ++i)
+			{
+				list->head->forward_nodes[i] = list->tail;
+				list->tail->forward_nodes[i] = list->head;
+			}
+		}
+		free(list->_help_trace_less_nodes);
+		list->_help_trace_less_nodes = (skip_list_node_t **)calloc(lvl + 1, sizeof(skip_list_node_t *));
+	}
+	list->using_max_lvl = lvl;
+}
+
+skip_list_node_t * skip_list_node_alloc(void * key, void * data, uint16_t  forward_node_size, skip_list_free_node_key_pt free_key)
 {
 	skip_list_node_t *ret = (skip_list_node_t *)calloc(1, sizeof(skip_list_node_t));
 	ret->key = key;
 	ret->free_key = free_key;
 	ret->data = data;
-	ret->lvl_node_size = lvl_node_size;
-	if (ret->lvl_node_size > 0)
-	{
-		ret->lvl_nodes = (skip_list_node_t **)calloc(ret->lvl_node_size, sizeof(skip_list_node_t *));
-	}
+	skip_list_node_check_expand_forward_nodes(ret, forward_node_size);
 	return ret;
 }
 
@@ -20,25 +55,33 @@ void skip_list_node_free(skip_list_node_t * node)
 	if (NULL == node)
 		return;
 
-	if (node->free_key)
+	if (node->free_key && node->key)
 	{
 		node->free_key(node->key);
 	}
-	if (node->lvl_nodes)
+	if (node->forward_nodes)
 	{
-		free(node->lvl_nodes);
+		free(node->forward_nodes);
 	}
 	free(node);
 }
 
-skip_list_t * skip_list_alloc(uint32_t expect_lvl, skip_list_cmp_node_key_pt cmp_key, skip_list_free_node_key_pt free_key)
+
+skip_list_t * skip_list_alloc(uint16_t expect_lvl, skip_list_cmp_node_key_pt cmp_key, skip_list_free_node_key_pt free_key)
 {
 	skip_list_t *ret = (skip_list_t *)calloc(1, sizeof(skip_list_t));
 	ret->expect_lvl = expect_lvl;
+	ret->using_max_lvl = 0;
+	ret->_help_trace_less_nodes = (skip_list_node_t **)calloc(ret->using_max_lvl + 1, sizeof(skip_list_node_t *));
 	ret->cmp_key = cmp_key;
 	ret->free_key = free_key;
-	ret->head = skip_list_node_alloc(NULL, NULL, ret->expect_lvl * 2, NULL);
-	ret->tail = skip_list_node_alloc(NULL, NULL, ret->expect_lvl * 2, NULL);
+	ret->head = skip_list_node_alloc(NULL, NULL, ret->expect_lvl * 2, ret->free_key);
+	ret->tail = skip_list_node_alloc(NULL, NULL, ret->expect_lvl * 2, ret->free_key);
+	ret->tail->back_node = ret->head;
+	for (uint16_t i = 0; i < ret->head->forward_node_size; ++i)
+	{
+		ret->head->forward_nodes[i] = ret->tail;
+	}
 	return ret;
 }
 
@@ -50,21 +93,63 @@ void skip_list_free(skip_list_t * list)
 	skip_list_node_t *p = list->tail;
 	while (p)
 	{
-		p = p->back_node;
-		skip_list_node_free(p);
+		skip_list_node_t *free_node = p;
+		p = free_node->back_node;
+		skip_list_node_free(free_node);
+		
 	}
-	skip_list_node_free(list->tail);
-	skip_list_node_free(list->head);
+	free(list->_help_trace_less_nodes);
 	free(list);
 }
 
-skip_list_node_t * skip_list_insert(skip_list_t * list, void * key, void * data)
+skip_list_node_t * skip_list_insert(skip_list_t * list, void * key, void * data) 
 {
-	return nullptr;
+	int node_max_level = 0;
+	int rand_val = rand() % list->expect_lvl;
+	while (0 != rand_val)
+	{
+		++node_max_level;
+		rand_val = rand() % list->expect_lvl;
+	}
+	skip_list_node_t *node = skip_list_node_alloc(key, data, node_max_level + 1, list->free_key);
+	
+	if (node_max_level > list->using_max_lvl)
+	{
+		skip_list_set_using_max_lvl(list, node_max_level);
+	}
+	int32_t using_max_lvl = list->using_max_lvl;
+	skip_list_node_t **record_less_nodes = list->_help_trace_less_nodes;
+
+	skip_list_node_t *curr_node = list->head;
+
+	for (int32_t curr_lvl = using_max_lvl; curr_lvl >= 0; --curr_lvl)
+	{
+		skip_list_node_t *cmp_node = curr_node->forward_nodes[curr_lvl];
+		while (cmp_node != list->tail && list->cmp_key(cmp_node->key, key))
+		{
+			curr_node = cmp_node;
+			cmp_node = curr_node->forward_nodes[curr_lvl];
+		}
+		record_less_nodes[curr_lvl] = curr_node;
+	}
+	
+	skip_list_node_t *pre_node = record_less_nodes[0];
+	skip_list_node_t *next_node = pre_node->forward_nodes[0];
+	next_node->back_node = node;
+	node->back_node = pre_node;
+
+	for (int32_t curr_lvl = 0; curr_lvl <= node_max_level; ++curr_lvl)
+	{
+		node->forward_nodes[curr_lvl] = record_less_nodes[curr_lvl]->forward_nodes[curr_lvl];
+		record_less_nodes[curr_lvl]->forward_nodes[curr_lvl] = node;
+	}
+
+	return node;
 }
 
 void skip_list_remove(skip_list_t * list, skip_list_node_t * node)
 {
+
 }
 
 skip_list_node_t * skip_list_find(skip_list_t * list, void * key)
--- server/third_party/data_struct/skip_list/skip_list.h
+++ server/third_party/data_struct/skip_list/skip_list.h
@@ -8,7 +8,7 @@ extern "C" {
 #endif
 
 typedef void(*skip_list_free_node_key_pt)(void *key);
-typedef void(*skip_list_cmp_node_key_pt)(void *key);
+typedef bool(*skip_list_cmp_node_key_pt)(void *a, void *b);
 
 typedef struct skip_list_node_s skip_list_node_t;
 struct skip_list_node_s
@@ -17,25 +17,26 @@ struct skip_list_node_s
 	skip_list_free_node_key_pt free_key;
 	void *data;
 	skip_list_node_t *back_node;
-	uint32_t lvl_node_size;
-	skip_list_node_t **lvl_nodes;
+	uint16_t forward_node_size;
+	skip_list_node_t **forward_nodes;
 };
 
 typedef struct skip_list_s skip_list_t;
 struct skip_list_s
 {
-	uint32_t expect_lvl;
-	uint32_t using_max_lvl;
+	uint16_t expect_lvl;
+	uint16_t using_max_lvl;
+	skip_list_node_t **_help_trace_less_nodes;
 	skip_list_node_t *head;
 	skip_list_node_t *tail;
 	skip_list_free_node_key_pt free_key;
-	skip_list_cmp_node_key_pt cmp_key;
+	skip_list_cmp_node_key_pt cmp_key; // cmp_key(a, b), return true a在前， 否则b在前
 };
 
-skip_list_node_t * skip_list_node_alloc(void * key, void * data, uint32_t lvl_node_size, skip_list_free_node_key_pt free_key);
+skip_list_node_t * skip_list_node_alloc(void * key, void * data, uint16_t forward_node_size, skip_list_free_node_key_pt free_key);
 void skip_list_node_free(skip_list_node_t *node);
 
-skip_list_t * skip_list_alloc(uint32_t expect_lvl, skip_list_cmp_node_key_pt cmp_key, skip_list_free_node_key_pt free_key);
+skip_list_t * skip_list_alloc(uint16_t expect_lvl, skip_list_cmp_node_key_pt cmp_key, skip_list_free_node_key_pt free_key);
 void skip_list_free(skip_list_t *list);
 skip_list_node_t * skip_list_insert(skip_list_t *list, void *key, void *data);
 void skip_list_remove(skip_list_t *list, skip_list_node_t *node);
