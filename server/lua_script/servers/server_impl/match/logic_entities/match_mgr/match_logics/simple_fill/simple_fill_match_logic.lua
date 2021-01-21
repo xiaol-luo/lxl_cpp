c92f0f79c5979693ba61fce672c674ac3215f9fa
luoxiaolong <luoxiaolong@corp.netease.com>
2021-01-21 20:48:03 +0800

    modify

--- server/lua_script/servers/server_impl/match/logic_entities/match_mgr/match_logics/simple_fill/simple_fill_match_logic.lua
+++ server/lua_script/servers/server_impl/match/logic_entities/match_mgr/match_logics/simple_fill/simple_fill_match_logic.lua
@@ -5,6 +5,7 @@ SimpleFillMatchLogic = SimpleFillMatchLogic or class("SimpleFillMatchLogic", Mat
 function SimpleFillMatchLogic:ctor(match_mgr, match_theme, logic_setting)
     SimpleFillMatchLogic.super.ctor(self, match_mgr, match_theme, logic_setting)
     self._last_do_match_timestamp = 0
+    self._Game_Min_Role_Num = 1
 end
 
 function SimpleFillMatchLogic:create_match_team(match_key, ask_role_id, teammate_role_ids, extra_param)
@@ -15,38 +16,49 @@ end
 
 function SimpleFillMatchLogic:_on_update()
     local now_sec = logic_sec()
-    if now_sec - self._last_do_match_timestamp >= 1 then
+    if now_sec - self._last_do_match_timestamp >= 2 then
         self._last_do_match_timestamp = now_sec
 
-        local consume_match_teams = {}
         local pre_key = nil
+        local consume_match_teams = {}
         repeat
-            --[[
-            local key_1, match_team_1 = next(self._key_to_team, pre_key)
-            if not key_1 then
-                break
-            end
-            --]]
-            local key_2, match_team_2 = next(self._key_to_team, pre_key)
-            if not key_2 then
-                break
-            end
-            pre_key = key_2
-            -- table.insert(consume_match_teams, key_1)
-            table.insert(consume_match_teams, key_2)
-            local match_game = MatchGameBase:new()
-            match_game.match_theme = self._match_theme
-            match_game.unique_key = gen_uuid()
-            table.insert(self._ready_match_games, match_game)
-            do
-                local team = match_team_2
+            local hit_team_keys = {}
+            local hit_teams = {}
+            local hit_team_role_num = 0
+            ---@type MatchTeamBase
+            local hit_team = nil
+            repeat
+                pre_key, hit_team = next(self._key_to_team, pre_key)
+                if not pre_key then
+                    break
+                end
+                table.insert(hit_team_keys, pre_key)
+                hit_team_role_num = hit_team_role_num + #hit_team.teammate_role_ids
+                table.insert(hit_teams, hit_team)
+                if hit_team_role_num >= self._Game_Min_Role_Num then -- 只要人数满足self._Game_Min_Role_Num，就打包成队
+                    break
+                end
+            until false
+
+            if hit_team_role_num >= self._Game_Min_Role_Num then
+                local match_game = MatchGameBase:new()
+                table.insert(self._ready_match_games, match_game)
+                match_game.match_theme = self._match_theme
+                match_game.unique_key = gen_uuid()
                 local match_camp = MatchCampBase:new()
-                match_camp.match_teams[team.match_key] = team
                 table.insert(match_game.match_camps, match_camp)
+                for _, team in ipairs(hit_teams) do
+                    match_camp.match_teams[team.match_key] = team
+                end
+                table.append(consume_match_teams, hit_team_keys)
             end
+
+            if not pre_key then
+                break
+            end
+
         until false
         for _, match_key in pairs(consume_match_teams) do
-            -- self._key_to_team[match_key] = nil
             self:leave_match(match_key)
         end
     end
