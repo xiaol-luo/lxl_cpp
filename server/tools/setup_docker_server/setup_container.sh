e8719faebeda4a4be54d5a29b24ab6936c734d92
xiaol-luo <xiaol.luo@163.com>
2021-01-31 18:58:59 +0800

    modify

--- server/tools/setup_docker_server/setup_container.sh
+++ server/tools/setup_docker_server/setup_container.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# mkdir -p /shared/docker/data_dir
+# git clone https://github.com/xiaol-luo/lxl_cpp.git  /shared/docker/data_dir/lxl_cpp
+# cd /shared/docker/data_dir/lxl_cpp
+# git checkout -b service_discovery  remotes/origin/service_discovery
+
 # mkdir -p /shared/docker
 # git clone https://github.com/xiaol-luo/docker_hub_build_centos7.git /shared/docker/docker_hub_build_centos7
 # cd /shared/docker/docker_hub_build_centos7
@@ -8,3 +13,4 @@
 docker rm -f test_lxl
 docker run --name test_lxl  -dit -v /shared/docker/data_dir/:/shared -p 30003:30003 -p 32003:32003 -p 31002:31002 -p 31005:31005 -p 35002:35002 -p 35005:35005 -p 42002:42002 -p 42005:42005 lxl_cxx/centos /bin/bash
 docker exec -it test_lxl /bin/bash
+docker exec test_lxl sh /shared/lxl_cpp/server/tools/setup_docker_server/setup_server.sh
